1cab546d5c3c6c8ae169c236b54a3e7c2da27390
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2022-10-26 13:05:41 +0300

    Kategoriler veri tabanından çekildi

--- YemekTarifiSitesi.Web/Kullanici.Master.cs
+++ YemekTarifiSitesi.Web/Kullanici.Master.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace YemekTarifiSitesi.Web
 {
     public partial class Kullanici : System.Web.UI.MasterPage
     {
+        private Sqlsinifi bgl = new Sqlsinifi();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            SqlCommand komut = new SqlCommand("SELECT * FROM Tbl_Kategoriler",bgl.baglanti());
+            SqlDataReader oku = komut.ExecuteReader();
+            DataList1.DataSource = oku;
+            DataList1.DataBind();
         }
     }
 }
--- YemekTarifiSitesi.Web/Kullanici.Master.designer.cs
+++ YemekTarifiSitesi.Web/Kullanici.Master.designer.cs
@@ -33,6 +33,15 @@ namespace YemekTarifiSitesi.Web
         protected global::System.Web.UI.HtmlControls.HtmlForm form2;
 
         /// <summary>
+        /// DataList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList DataList1;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
