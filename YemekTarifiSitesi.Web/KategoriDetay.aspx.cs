b10a3973c3582969be7d9f37a65a7a4447328a78
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2022-10-31 16:09:50 +0300

    1)Kategori Detay sayfası oluşturuldu 2)Kullanici master sayfasına kategoriId eklendi ve link verildi 3)Kullanici master sayfasına banner eklendi. 4)Kategori detay sayfası tıpkı anasayfa gibi veriler çekilde fakat bu sefer o kategorinin idsine göre işlem yapıyor.

---
+++ YemekTarifiSitesi.Web/KategoriDetay.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace YemekTarifiSitesi.Web
+{
+    public partial class KategoriDetay : System.Web.UI.Page
+    {
+        Sqlsinifi bgl = new Sqlsinifi();
+        string kategoriId = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            kategoriId = Request.QueryString["kategoriId"];
+            SqlCommand komut = new SqlCommand("SELECT * FROM Tbl_Yemekler WHERE KategoriId = @KategoriId", bgl.baglanti());
+            komut.Parameters.AddWithValue("@KategoriId", kategoriId);
+            SqlDataReader dr = komut.ExecuteReader();
+            DataList2.DataSource = dr;
+            DataList2.DataBind();
+        }
+    }
+}
--- YemekTarifiSitesi.Web/Kullanici.Master.designer.cs
+++ YemekTarifiSitesi.Web/Kullanici.Master.designer.cs
@@ -33,6 +33,15 @@ namespace YemekTarifiSitesi.Web
         protected global::System.Web.UI.HtmlControls.HtmlForm form2;
 
         /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+
+        /// <summary>
         /// DataList1 control.
         /// </summary>
         /// <remarks>
